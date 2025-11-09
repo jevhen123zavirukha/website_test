518afb481b0c334a2729230fd2e177987590128a
Eugene Zavirukha <jevhenzavirukha@gmail.com>
2025-11-09 19:03:55 +0100

    Revert "Master"

--- mysite/main/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class AccessForm(forms.Form):
-    email = forms.EmailField(label='Email')
-    password = forms.CharField(widget=forms.PasswordInput, label='Password')
